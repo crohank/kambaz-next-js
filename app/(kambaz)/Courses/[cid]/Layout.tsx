5a417743f5b29cabc9f17c28f46652b885eee4b6
Rohan Kumar <crohank02@gmail.com>
2025-09-17 02:36:54 -0400

    Update Layout.tsx

--- app/(kambaz)/Courses/[cid]/Layout.tsx
+++ app/(kambaz)/Courses/[cid]/Layout.tsx
@@ -1,3 +1,34 @@
+// import { ReactNode } from "react";
+// import CourseNavigation from "./Navigation";
+
+// export default function CoursesLayout({
+//   children,
+//   params,
+// }: {
+//   children: ReactNode;
+//   params: { cid: string }; 
+// }) {
+//   const { cid } = params; 
+
+//   return (
+//     <div id="wd-courses">
+//       <h2>Courses {cid}</h2>
+//       <hr />
+//       <table>
+//         <tbody>
+//           <tr>
+//             <td valign="top" width="200">
+//               <CourseNavigation />
+//             </td>
+//             <td valign="top" width="100%">
+//               {children}
+//             </td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// }
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
 
@@ -6,9 +37,9 @@ export default function CoursesLayout({
   params,
 }: {
   children: ReactNode;
-  params: { cid: string }; 
+  params: { cid: string };
 }) {
-  const { cid } = params; 
+  const { cid } = params;
 
   return (
     <div id="wd-courses">
