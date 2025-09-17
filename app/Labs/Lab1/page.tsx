a5d166f0909b30ddb399926141b87bc0b65c608a
Rohan Kumar <crohank02@gmail.com>
2025-09-17 00:29:21 -0400

    MOdified the <Img> tag to <Image>

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -1,4 +1,5 @@
-"use client"
+"use client";
+import Image from "next/image";
 export default function Lab1() {
   return (
     <div id="wd-lab1">
@@ -148,15 +149,23 @@ export default function Lab1() {
       <div id="wd-images">
         <h4>Image tag</h4>
         Loading an image from the internet: <br />
-        <img
+        <Image
           id="wd-starship"
-          width="400px"
           src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+          alt="Starship"
+          width={400}
+          height={200} 
         />
         <br />
         Loading a local image:
         <br />
-        <img id="wd-teslabot" src="/images/teslabot.jpg" height="200px" />
+        <Image
+  id="wd-teslabot"
+  src="/images/teslabot.jpg"
+  alt="Tesla Bot"
+  width={200}
+  height={200} 
+/>
       </div>
       <div id="wd-forms">
         <h4>Form Elements</h4>
@@ -210,100 +219,130 @@ export default function Lab1() {
       <div>
         <h5 id="wd-radio-buttons">Radio buttons</h5>
 
-<label>Favorite movie genre:</label><br />
+        <label>Favorite movie genre:</label>
+        <br />
 
-<input type="radio" name="radio-genre" id="wd-radio-comedy"/>
-<label htmlFor="wd-radio-comedy">Comedy</label><br />
+        <input type="radio" name="radio-genre" id="wd-radio-comedy" />
+        <label htmlFor="wd-radio-comedy">Comedy</label>
+        <br />
 
-<input type="radio" name="radio-genre" id="wd-radio-drama"/>
-<label htmlFor="wd-radio-drama">Drama</label><br />
+        <input type="radio" name="radio-genre" id="wd-radio-drama" />
+        <label htmlFor="wd-radio-drama">Drama</label>
+        <br />
 
-<input type="radio" name="radio-genre" id="wd-radio-scifi"/>
-<label htmlFor="wd-radio-scifi">Science Fiction</label><br />
-<input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
-<label htmlFor="wd-radio-fantasy">Fantasy</label>
-
-
+        <input type="radio" name="radio-genre" id="wd-radio-scifi" />
+        <label htmlFor="wd-radio-scifi">Science Fiction</label>
+        <br />
+        <input type="radio" name="radio-genre" id="wd-radio-fantasy" />
+        <label htmlFor="wd-radio-fantasy">Fantasy</label>
       </div>
 
       <div>
         <h5 id="wd-checkboxes">Checkboxes</h5>
-<label>Favorite movie genre:</label><br/>
+        <label>Favorite movie genre:</label>
+        <br />
 
-<input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
-<label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+        <input type="checkbox" name="check-genre" id="wd-chkbox-comedy" />
+        <label htmlFor="wd-chkbox-comedy">Comedy</label>
+        <br />
 
-<input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
-<label htmlFor="wd-chkbox-drama">Drama</label><br/>
+        <input type="checkbox" name="check-genre" id="wd-chkbox-drama" />
+        <label htmlFor="wd-chkbox-drama">Drama</label>
+        <br />
 
-<input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
-<label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+        <input type="checkbox" name="check-genre" id="wd-chkbox-scifi" />
+        <label htmlFor="wd-chkbox-scifi">Science Fiction</label>
+        <br />
 
-<input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
-<label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+        <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy" />
+        <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
       </div>
       <div>
         <h4 id="wd-dropdowns">Dropdowns</h4>
 
-<h5>Select one</h5>
-<label  htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
-<select id="wd-select-one-genre">
-   <option value="COMEDY">Comedy</option>
-   <option value="DRAMA">Drama</option>
-   <option selected value="SCIFI">
-       Science Fiction</option>
-   <option value="FANTASY">Fantasy</option>
-   </select>
-
+        <h5>Select one</h5>
+        <label htmlFor="wd-select-one-genre"> Favorite movie genre: </label>
+        <br />
+        <select id="wd-select-one-genre">
+          <option value="COMEDY">Comedy</option>
+          <option value="DRAMA">Drama</option>
+          <option selected value="SCIFI">
+            Science Fiction
+          </option>
+          <option value="FANTASY">Fantasy</option>
+        </select>
       </div>
-      <div> 
+      <div>
         <h5>Select many</h5>
-<label  htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
-<select multiple id="wd-select-many-genre">
-   <option value="COMEDY" selected> Comedy          </option>
-   <option value="DRAMA">           Drama           </option>
-   <option value="SCIFI"  selected> Science Fiction </option>
-   <option value="FANTASY">         Fantasy         </option>
-</select>
+        <label htmlFor="wd-select-many-genre"> Favorite movie genres: </label>
+        <br />
+        <select multiple id="wd-select-many-genre">
+          <option value="COMEDY" selected>
+            {" "}
+            Comedy{" "}
+          </option>
+          <option value="DRAMA"> Drama </option>
+          <option value="SCIFI" selected>
+            {" "}
+            Science Fiction{" "}
+          </option>
+          <option value="FANTASY"> Fantasy </option>
+        </select>
       </div>
 
-      <div> 
+      <div>
         <h4>Other HTML field types</h4>
 
-<label htmlFor="wd-text-fields-email"> Email: </label>
-<input type="email"
-       placeholder="crohank02@gmail.com"
-       id="wd-text-fields-email"/><br/>
+        <label htmlFor="wd-text-fields-email"> Email: </label>
+        <input
+          type="email"
+          placeholder="crohank02@gmail.com"
+          id="wd-text-fields-email"
+        />
+        <br />
 
-<label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
-<input type="number"
-       value="100000"
-       placeholder="1000"
-       id="wd-text-fields-salary-start"/><br/>
+        <label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
+        <input
+          type="number"
+          value="100000"
+          placeholder="1000"
+          id="wd-text-fields-salary-start"
+        />
+        <br />
 
-<label htmlFor="wd-text-fields-rating"> Rating: </label>
-<input type="range"
-       value="4"
-       max="5"
-       placeholder="Doe"
-       id="wd-text-fields-rating"/><br/>
+        <label htmlFor="wd-text-fields-rating"> Rating: </label>
+        <input
+          type="range"
+          value="4"
+          max="5"
+          placeholder="Doe"
+          id="wd-text-fields-rating"
+        />
+        <br />
 
-<label htmlFor="wd-text-fields-dob"> Date of birth: </label>
-<input type="date"
-       value="2000-01-21"
-       id="wd-text-fields-dob"/><br/>
+        <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+        <input type="date" value="2000-01-21" id="wd-text-fields-dob" />
+        <br />
       </div>
       <div>
         <h4>Anchor tag</h4>
-Please
-<a href="https://www.lipsum.com" id="wd-lipsum">click here</a>
-to get dummy text<br/>
+        Please
+        <a href="https://www.lipsum.com" id="wd-lipsum">
+          click here
+        </a>
+        to get dummy text
+        <br />
       </div>
       <div>
         <h4>Github</h4>
-<p>Please <a href="https://github.com/crohank?tab=repositories" id="wd-github">click here</a>
-to open github link</p> <br/>
-
+        <p>
+          Please{" "}
+          <a href="https://github.com/crohank?tab=repositories" id="wd-github">
+            click here
+          </a>
+          to open github link
+        </p>{" "}
+        <br />
       </div>
     </div>
   );
