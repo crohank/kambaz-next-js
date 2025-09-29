174436738a7283dc58eeef5542011b17dca7e1ad
Rohan Kumar <crohank02@gmail.com>
2025-09-28 23:38:26 -0400

    Added Float Images

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -1,10 +1,14 @@
 import BackgroundColors from "./BackgroundColors";
 import Borders from "./Borders";
 import Corners from "./Corners";
+import Dimensions from "./Dimensions";
+import Float from "./Float";
 import ForegroundColors from "./ForegroundColors";
 import "./index.css";
 import Margins from "./Margins";
 import Padding from "./Padding";
+import Positions from "./Positions";
+import Zindex from "./Zindex";
 export default function Lab2() {
   return (
     <div id="wd-lab2">
@@ -73,6 +77,18 @@ export default function Lab2() {
       </div>
       <div>
         <Corners />
+      </div>
+      <div>
+        <Dimensions/>
+      </div>
+      <div>
+        <Positions/>
+      </div>
+      <div>
+        <Zindex/>
+      </div>
+      <div>
+        <Float/>
       </div>
     </div>
   );
