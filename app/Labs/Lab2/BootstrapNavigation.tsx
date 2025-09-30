37baee4c0398861dbed0ecc6bb2af6bbb397fc4d
Rohan Kumar <crohank02@gmail.com>
2025-09-29 21:42:34 -0400

    Kambaz css

--- app/Labs/Lab2/BootstrapNavigation.tsx
+++ app/Labs/Lab2/BootstrapNavigation.tsx
@@ -1,10 +1,10 @@
 import {
-  Row,
-  FormLabel,
-  Col,
-  FormCheck,
   Button,
-  Form,
+  Card,
+  CardBody,
+  CardImg,
+  CardText,
+  CardTitle,
   Nav,
   NavItem,
   NavLink,
@@ -12,6 +12,7 @@ import {
 
 export default function BootstrapNavigation() {
   return (
+    <div>
     <div id="wd-css-navigating-with-tabs">
       <h2>Tabs</h2>
       <Nav variant="tabs">
@@ -30,6 +31,21 @@ export default function BootstrapNavigation() {
           </NavLink>
         </NavItem>
       </Nav>
+    </div>
+    <div id="wd-css-navigating-with-cards">
+  <h2> Cards </h2>
+  <Card style={{ width: "18rem" }}>
+    <CardImg variant="top" src="/images/stacked.jpg" />
+    
+    <CardBody>
+      <CardTitle>Stacking Starship</CardTitle>
+      <CardText>
+        Stacking the most powerful rocket in history. Mars or bust!
+      </CardText>
+      <Button variant="primary">Boldly Go</Button>
+    </CardBody>
+    </Card>
+    </div>
     </div>
   );
 }
