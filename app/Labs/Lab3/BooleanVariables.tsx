0faf660ca064c1471b6fa0e1f24311cdeef2b0cb
Rohan Kumar <crohank02@gmail.com>
2025-10-11 00:34:58 -0400

    Update BooleanVariables.tsx

--- app/Labs/Lab3/BooleanVariables.tsx
+++ app/Labs/Lab3/BooleanVariables.tsx
@@ -1,13 +1,12 @@
 export default function booleanVariable() {
-  const numberVariable = 123,
-    floatingPointNumber = 234.345;
+  const numberVariable = 123;
   const true1 = true,
     false1 = false;
   const false2 = true1 && false1;
   const true2 = true1 || false1;
   const true3 = !false2;
   const true4 = numberVariable === 123; // always use === not ==
-  const true5 = floatingPointNumber !== 321.432;
+  const true5 = true;
   const false3 = numberVariable < 100;
   return (
     <div id="wd-boolean-variables">
