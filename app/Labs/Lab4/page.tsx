4eb90857e569af323c0e48853ecbeeac7ab11f8e
Rohan Kumar <crohank02@gmail.com>
2025-10-23 11:21:56 -0400

    Page 112

--- app/Labs/Lab4/page.tsx
+++ app/Labs/Lab4/page.tsx
@@ -1,8 +1,14 @@
 "use client"
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariables";
 import ClickEvent from "./ClickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
 import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
 
 export default function lab4() {
   function sayHello() {
@@ -16,6 +22,12 @@ export default function lab4() {
       <PassingDataOnEvent />
       <PassingFunctions theFunction={sayHello} />
       <EventObject/>
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ObjectStateVariable/>
+      <ArrayStateVariable/>
     </div>
   );
 }
