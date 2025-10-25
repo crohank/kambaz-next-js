58e3399f8d1604d070a5f2b5f93b5bdc801b1115
Rohan Kumar <crohank02@gmail.com>
2025-10-25 08:45:37 -0400

    Update page.tsx

--- app/Labs/Lab4/ReduxExamples/AddRedux/page.tsx
+++ app/Labs/Lab4/ReduxExamples/AddRedux/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
 import { add } from "./addReducer";
@@ -8,6 +7,7 @@ import { Button } from "react-bootstrap";
 export default function AddRedux() {
   const [a, setA] = useState(12);
   const [b, setB] = useState(23);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const { sum } = useSelector((state: any) => state.addReducer);
   const dispatch = useDispatch();
   return (
