fa740ec2672ccd6541ff929e54baf0d91791ab2f
Rohan Kumar <crohank02@gmail.com>
2025-10-25 09:12:54 -0400

    Update page.tsx

--- app/(kambaz)/Dashboard/page.tsx
+++ app/(kambaz)/Dashboard/page.tsx
@@ -113,6 +113,7 @@ export default function Dashboard() {
           onChange={(e) => setCourse({ ...course, name: e.target.value })}
         />
         <FormControl
+        as={"textarea"}
           value={course.description}
           rows={3}
           onChange={(e) =>
