be076ff0fd7d3c58d1eb44640f72ed24a9c63398
Rohan Kumar <crohank02@gmail.com>
2025-10-29 18:21:39 -0400

    Updated ArrayStateVariable

--- app/Labs/Lab4/ArrayStateVariable.tsx
+++ app/Labs/Lab4/ArrayStateVariable.tsx
@@ -1,28 +1,42 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-import {useState} from "react";
-import {ListGroup, ListGroupItem} from "react-bootstrap";
-import {useSelector} from "react-redux";
+import { useState } from "react";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 export default function ArrayStateVariable() {
-    const {todos} = useSelector((state: any) => state.todosReducer);
-    const [array, setArray] = useState([1, 2, 3, 4, 5]);
-    const addElement = () => {
-        setArray([...array, Math.floor(Math.random() * 100)]);
-    };
-    return (
-        <div id="wd-array-state-variables">
-            <h2>Array State Variable</h2>
-            <button className="btn btn-primary" onClick={addElement}>Add Element</button>
-            <ListGroup>
-                {todos.map((todo: any) => (
-                    <ListGroupItem key={todo.id}>
-                        {todo.title}
-                    </ListGroupItem>
-                ))}
-            </ListGroup>
-            <hr/>
-            <hr/>
-        </div>
-    );
+  const { todos } = useSelector((state: any) => state.todosReducer);
+  const [array, setArray] = useState([1, 2, 3, 4, 5]);
+  const addElement = () => {
+    setArray([...array, Math.floor(Math.random() * 100)]);
+  };
+
+  const deleteElement = (index: number) => {
+    setArray(array.filter((item, i) => i !== index));
+  };
+
+  return (
+    <div id="wd-array-state-variables">
+      <h2>Array State Variable</h2>
+      <button className="btn btn-primary" onClick={addElement}>
+        Add Element
+      </button>
+      <ListGroup>
+        {todos.map((todo: any) => (
+          <ListGroupItem key={todo.id}>{todo.title}</ListGroupItem>
+        ))}
+      </ListGroup>
+      <hr />
+      <ul>
+        {array.map((item, index) => (
+          <li key={index}>
+            {" "}
+            {item}
+            <button onClick={() => deleteElement(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <hr />
+    </div>
+  );
 }
